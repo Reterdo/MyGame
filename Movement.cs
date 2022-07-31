53eb310486c85cfdce986246feb1429a59ecdc51
Reterdo <ivantsov.pasha@bk.ru>
2022-07-31 19:07:50 +0400

    Movement
    31/07/2022

--- Movement.cs
+++ Movement.cs
@@ -3,16 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{ 
+    public CharacterController controller;
+    private Animator anim;
+
+    public float speed = 6f;
+    
+    public float turnSmoothTme = 0.1f;
+    float turnSmoothVelocity;
+
+    void start()
     {
-        
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.y);
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTme);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 MoveDir = Quaternion.Euler(0f, targetAngle, of);
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }    
 }
--- PlayerCamera.cs
+++ PlayerCamera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameCamera : MonoBehaviour
+public class PlayerCamera : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
